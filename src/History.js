c47ef003ede68b32e9766a601221542725f6d44f
Klas Reevcrona <klas.reevcrona@learnet.se>
2024-08-12 14:38:40 +0200

    commit

--- src/History.js
+++ src/History.js
@@ -5,7 +5,7 @@ import React from "react"
 function History(props){
     return(
         <div onClick={() => props.find(props.id)} className="tab-container">
-            <p >{props.expression} = {props.result}</p>
+            <p className="tab-p">{props.expression} = {props.result}</p>
             
         </div>
     )
